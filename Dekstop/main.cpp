adcdf43e682226d2f830eae5efc49e917dd332bf
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-12-06 11:50:52 +0300

    Небольшой тест в основном методе.

--- Dekstop/main.cpp
+++ Dekstop/main.cpp
@@ -1,4 +1,5 @@
 #include "mainwindow.h"
+#include "WebInteraction/webcontext.h"
 
 #include <QApplication>
 
@@ -7,5 +8,9 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    WebContext webContext;
+    webContext.sendRequest("https://catfact.ninja/fact", QList<QString>{});
+
     return a.exec();
 }
