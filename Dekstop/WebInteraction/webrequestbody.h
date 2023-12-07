9166e9028c523a6b5535c3396757dfdc41b0bdb9
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-12-07 10:47:08 +0300

    Было решено добавить универсальное поле для хранения данных тела запроса.

--- Dekstop/WebInteraction/webrequestbody.h
+++ Dekstop/WebInteraction/webrequestbody.h
@@ -2,6 +2,10 @@
 #define WEBREQUESTBODY_H
 
 #include <QObject>
+#include <QVariant>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QJsonParseError>
 
 ///
 /// \brief The WebRequestBody class
@@ -10,6 +14,10 @@
 class WebRequestBody : public QObject
 {
     Q_OBJECT
+
+private:
+    QVariant data;
+
 public:
     explicit WebRequestBody(QObject *parent = nullptr);
 
