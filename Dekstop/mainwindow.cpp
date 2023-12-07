021d09e06311ac7d5079bf41c8245ff40a741a49
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-12-07 13:40:13 +0300

    Теперь класс основного окна имеет поле ВебКонтекста.

--- Dekstop/mainwindow.cpp
+++ Dekstop/mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    webContext = new WebContext(this);
+    ui->webRequestWidget->setWebContext(webContext);
 }
 
 MainWindow::~MainWindow()
