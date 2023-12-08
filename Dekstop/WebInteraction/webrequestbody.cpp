a307e56afab222be9e3e072f0bf62ec86cf83693
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-12-08 15:46:43 +0300

    Для корректной работы класса WebRequestBody был добавлен новый конструктор, а также геттер, не привязанный к типу данных!

--- Dekstop/WebInteraction/webrequestbody.cpp
+++ Dekstop/WebInteraction/webrequestbody.cpp
@@ -5,6 +5,11 @@ QJsonObject WebRequestBody::getJsonData() const
     return qvariant_cast<QJsonObject>(data);
 }
 
+QVariant WebRequestBody::getData() const
+{
+    return data;
+}
+
 void WebRequestBody::setJsonData(const QJsonObject &newJsonData)
 {
     data = QVariant::fromValue(newJsonData);
@@ -12,5 +17,10 @@ void WebRequestBody::setJsonData(const QJsonObject &newJsonData)
 
 WebRequestBody::WebRequestBody(QObject *parent)
     : QObject{parent}
+{
+}
+
+WebRequestBody::WebRequestBody(const WebRequestBody &other) :
+    data(other.getData())
 {
 }
--- Dekstop/WebInteraction/webrequestbody.h
+++ Dekstop/WebInteraction/webrequestbody.h
@@ -15,15 +15,16 @@ class WebRequestBody : public QObject
 
 public:
     explicit WebRequestBody(QObject *parent = nullptr);
+    WebRequestBody(const WebRequestBody &other);
 
     QJsonObject getJsonData() const;
+    QVariant getData() const;
 
 private:
     QVariant data;
 
 private slots:
     void setJsonData(const QJsonObject &newJsonData);
-
 };
 
 #endif // WEBREQUESTBODY_H
