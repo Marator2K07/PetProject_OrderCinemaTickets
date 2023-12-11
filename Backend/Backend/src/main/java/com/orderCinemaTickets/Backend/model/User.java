016bb6b9a1885d04aa256e45587398ec07585bc7
Mikhail Belozerov <belozerov.mg@gmail.com>
2023-12-11 16:36:15 +0700

    Added CRUD methods in UserController

--- Backend/Backend/src/main/java/com/orderCinemaTickets/Backend/model/User.java
+++ Backend/Backend/src/main/java/com/orderCinemaTickets/Backend/model/User.java
@@ -1,5 +1,6 @@
 package com.orderCinemaTickets.Backend.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
