ca95b853bb0fe070f18b436e7b6e9b92177c2dbb
Mikhail Belozerov <belozerov.mg@gmail.com>
2023-12-13 19:00:29 +0700

    Added a list of users to frontend

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './layout/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-        <Navbar>
-          
-        </Navbar>
+      <Router>
+
+        <Navbar />
+
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+        </Routes>
+
+
+      </Router>
     </div>
   );
 }
