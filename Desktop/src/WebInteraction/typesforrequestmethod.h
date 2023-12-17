76d7b7d5ce710a1de0b4f6fd0a7e45c653fb97f7
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-12-17 16:02:06 +0300

    Создание нового класса под перечисления.
    Теперь перечисления для веб запросов находятся в классе TypesForRequestMethod.

--- Desktop/src/WebInteraction/typesforrequestmethod.h
+++ Desktop/src/WebInteraction/typesforrequestmethod.h
@@ -1,12 +1,20 @@
 #ifndef TYPESFORREQUESTMETHOD_H
 #define TYPESFORREQUESTMETHOD_H
 
-enum MethodType {
-    GET = 0, POST = 1
-};
+#include <QObject>
 
-enum MethodBodyType {
-    TEXT = 0, JSON = 1
+class TypesForRequestMethod : public QObject
+{
+    Q_OBJECT
+public:
+    enum MethodType {
+        GET = 0, POST = 1
+    };
+    Q_ENUM(MethodType)
+    enum MethodBodyType {
+        TEXT = 0, JSON = 1
+    };
+    Q_ENUM(MethodBodyType)
 };
 
 #endif // TYPESFORREQUESTMETHOD_H
