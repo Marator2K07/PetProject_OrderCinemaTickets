c987afedc150bb5cc1c3e1967730ed0f502f20c8
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-12-29 12:49:19 +0300

    Update IWebRequestInfo.h
    Раз это интерфейс, то не забываем указывать функции, как чисто виртуальные.

--- Desktop/backend/webrequestmodel/IWebRequestInfo.h
+++ Desktop/backend/webrequestmodel/IWebRequestInfo.h
@@ -16,26 +16,26 @@ class IWebRequestInfo
 {
 public:
     // каждый запрос должен иметь тип запроса
-    virtual void setRequestType(const RequestType::State &type);
-    virtual RequestType::State requestType() const;
+    virtual void setRequestType(const RequestType::State &type) = 0;
+    virtual RequestType::State requestType() const = 0;
     // каждый запрос может иметь тип тела запроса
-    virtual void setRequestBodyType(const RequestBodyType::State &type);
-    virtual RequestBodyType::State requestBodyType() const;
+    virtual void setRequestBodyType(const RequestBodyType::State &type) = 0;
+    virtual RequestBodyType::State requestBodyType() const = 0;
     // каждый запрос должен иметь адрес
-    virtual void setUrl(const QString &url);
-    virtual QString url() const;
+    virtual void setUrl(const QString &url) = 0;
+    virtual QString url() const = 0;
     // каждый запрос может иметь данные запроса
-    virtual void setData(const QVariant &data);
-    virtual QVariant data() const;
+    virtual void setData(const QVariant &data) = 0;
+    virtual QVariant data() const = 0;
 
     // самый главный метод - метод отправки данных запроса
-    virtual void send(const IWebRequestInfo *);
+    virtual void send(const IWebRequestInfo *) = 0;
 
     virtual ~IWebRequestInfo() {}
 
 private:
     // должна существовать возможность проверить запрос
-    virtual bool isValid();
+    virtual bool isValid() = 0;
 };
 
 #endif // IWEBREQUESTINFO_H
