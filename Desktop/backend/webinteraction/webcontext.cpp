2f307fe2ad05bf5cb51cc692a5d6bdcd7dd3ddee
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-12-31 08:12:56 +0300

    Обновления в классе webcontext.
    Заполняем уже старые известные методы, но пока без реализации, т.к. она может отличаться.

--- Desktop/backend/webinteraction/webcontext.cpp
+++ Desktop/backend/webinteraction/webcontext.cpp
@@ -5,3 +5,30 @@ WebContext::WebContext(QObject *parent)
 {
 
 }
+
+void WebContext::ignoreSslVerify()
+{
+    QSslConfiguration config = QSslConfiguration::defaultConfiguration();
+    config.setPeerVerifyMode(QSslSocket::VerifyNone);
+    QSslConfiguration::setDefaultConfiguration(config);
+}
+
+void WebContext::sendGetRequest(IWebRequestInfo *info)
+{
+
+}
+
+void WebContext::sendPostRequest(IWebRequestInfo *info)
+{
+
+}
+
+QByteArray WebContext::handleRequestDataAsString(QVariant data)
+{
+
+}
+
+QByteArray WebContext::handleRequestDataAsJson(QVariant data)
+{
+
+}
--- Desktop/backend/webinteraction/webcontext.h
+++ Desktop/backend/webinteraction/webcontext.h
@@ -1,7 +1,17 @@
 #ifndef WEBCONTEXT_H
 #define WEBCONTEXT_H
 
+#include "RequestType.h"
+#include "RequestBodyType.h"
+#include "IWebRequestInfo.h"
+
+#include <QDebug>
 #include <QObject>
+#include <QByteArray>
+#include <QDataStream>
+#include <QJsonDocument>
+#include <QSslConfiguration>
+#include <QNetworkAccessManager>
 
 ///
 /// \brief The WebContext class
@@ -11,6 +21,26 @@ class WebContext : public QObject
     Q_OBJECT
 public:
     explicit WebContext(QObject *parent = nullptr);
+    ///
+    /// \brief ignoreSslVerify
+    /// дабы избежать множество проблем
+    /// можно отключить/игнорировать Ssl верификацию
+    void ignoreSslVerify();
+
+private:
+    QNetworkAccessManager *webManager;
+
+    // указатель на метод отправки запроса:
+    // (методы: sendGetRequest или sendPostRequest)
+    void(WebContext::*requestMethod)(IWebRequestInfo *);
+    void sendGetRequest(IWebRequestInfo *info);
+    void sendPostRequest(IWebRequestInfo *info);
+
+    // указатель на метод обработки данных в определенном формате
+    // (методы: handleRequestDataAsString и handleRequestDataAsJson)
+    QByteArray(WebContext::*handleRequestDataMethod)(QVariant);
+    QByteArray handleRequestDataAsString(QVariant data);
+    QByteArray handleRequestDataAsJson(QVariant data);
 
 signals:
 
