70f176dcf8912ceaecbb0e557269381afd288939
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-01-02 16:19:53 +0300

    Create IWebResponceModel.h
    Создан интерфейс представления модели веб ответа. Предположительная версия.

---
+++ Desktop/backend/webresponcemodel/IWebResponceModel.h
@@ -0,0 +1,26 @@
+#ifndef IWEBRESPONCEMODEL_H
+#define IWEBRESPONCEMODEL_H
+
+#include <QNetworkReply>
+#include <QByteArray>
+
+///
+/// \brief The IWebResponceModel class
+/// интерфейс для организации структуры
+/// данных и функциональности какого-либо веб ответа
+class IWebResponceModel
+{
+public:
+    // каждый ответ может иметь тип возможной ошибки
+    virtual void setError(const QString &error) = 0;
+    virtual QString error() const = 0;
+    // каждый ответ может иметь полученные данные в виде массива байтов
+    virtual void setData(const QByteArray &data) = 0;
+    virtual QByteArray data() const = 0;
+    // должна существовать возможность проверить ответ на "испорченность"
+    virtual bool withErrors() = 0;
+
+    virtual ~IWebResponceModel() {}
+};
+
+#endif // IWEBRESPONCEMODEL_H
