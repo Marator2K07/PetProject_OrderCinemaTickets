9bc5a6a97673d9ff3fef3ce6d876c3708c329462
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-01-02 18:33:48 +0300

    Замена в библиотеке webinteraction.
    Добавлен заголовочный файл RequestEnums, функционально заменяющий три файла: RequestBodyType, RequestRoot и RequestType.

---
+++ Desktop/backend/webinteraction/RequestEnums.h
@@ -0,0 +1,31 @@
+#ifndef REQUESTENUMS_H
+#define REQUESTENUMS_H
+
+#include <QObject>
+
+class RequestEnums {
+    Q_GADGET
+public:
+    // для типа запроса
+    enum Type {
+        GET = 0,
+        POST,
+        TYPENONE
+    };
+    Q_ENUM(Type)
+    // для типа данных в запросе
+    enum DataType {
+        TEXT = 0,
+        JSON,
+        DATANONE
+    };
+    Q_ENUM(DataType)
+    // для идентфикации данного запроса
+    // во время дальнейшей обработки
+    enum Identifier {
+        REQUESTFORM = 0
+    };
+    Q_ENUM(Identifier)
+};
+
+#endif // REQUESTENUMS_H
