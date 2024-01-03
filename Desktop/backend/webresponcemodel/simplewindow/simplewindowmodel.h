d62f36e1d15c2261e85c1e97783ba9b11bde6f1d
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-01-03 17:18:56 +0300

    Обновления в классе simplewindowmodel.
    Добавлена минимальная реализация для исправной функциональности.

--- Desktop/backend/webresponcemodel/simplewindow/simplewindowmodel.h
+++ Desktop/backend/webresponcemodel/simplewindow/simplewindowmodel.h
@@ -6,7 +6,7 @@
 #include <QtQml>
 
 ///
-/// \brief The SimpleWindowInfo class
+/// \brief The SimpleWindowModel class
 /// модель данных для простого окна просмотра
 /// веб-ответа без какого-либо форматирования
 class SimpleWindowModel : public QObject,
@@ -14,12 +14,28 @@ class SimpleWindowModel : public QObject,
 {
     Q_OBJECT
     QML_ELEMENT
+    Q_PROPERTY(QString error READ error WRITE setError NOTIFY errorChanged)
+    Q_PROPERTY(QByteArray data READ data WRITE setData NOTIFY dataChanged)
 
 public:
     explicit SimpleWindowModel(QObject *parent = nullptr);
+    SimpleWindowModel(const SimpleWindowModel &other);
+
+    // IWebResponceModel interface {
+    void setError(const QString &error) override;
+    QString error() const override;
+    void setData(const QByteArray &data) override;
+    QByteArray data() const override;
+    ~SimpleWindowModel() override;
+    // }
+
+private:
+    QString m_error;
+    QByteArray m_data;
 
 signals:
-
+    void errorChanged(QString);
+    void dataChanged(QByteArray);
 };
 
 #endif // SIMPLEWINDOWMODEL_H
