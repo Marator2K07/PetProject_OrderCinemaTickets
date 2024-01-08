1598921f424f42b0db2f26607257d4b5988e53a7
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-01-08 13:42:05 +0300

    Обновления в библиотеке loggermodel.
    Написана предполагаемая реализация, как наследника интерфейса  ILoggerModel, кроме метода log.

--- Desktop/backend/loggermodel/windowlogger/windowloggermodel.cpp
+++ Desktop/backend/loggermodel/windowlogger/windowloggermodel.cpp
@@ -2,6 +2,42 @@
 
 WindowLoggerModel::WindowLoggerModel(QObject *parent)
     : QObject{parent}
-{
+{    
+}
 
+WindowLoggerModel::WindowLoggerModel(const WindowLoggerModel &other)
+    : m_path{other.path()}
+    , m_messages{other.messages()}
+{
+}
+
+void WindowLoggerModel::setPath(const QString &path)
+{
+    if (path != m_path) {
+        m_path = path;
+        emit pathChanged(path);
+    }
+}
+
+QString WindowLoggerModel::path() const
+{
+    return m_path;
+}
+
+void WindowLoggerModel::setMessages(const QQueue<QString> &messages)
+{
+    if (messages != m_messages) {
+        m_messages = messages;
+        emit messagesChanged(messages);
+    }
+}
+
+QQueue<QString> WindowLoggerModel::messages() const
+{
+    return m_messages;
+}
+
+void WindowLoggerModel::log(QString)
+{
+    
 }
--- Desktop/backend/loggermodel/windowlogger/windowloggermodel.h
+++ Desktop/backend/loggermodel/windowlogger/windowloggermodel.h
@@ -1,18 +1,39 @@
 #ifndef WINDOWLOGGERMODEL_H
 #define WINDOWLOGGERMODEL_H
 
+#include <ILoggerModel.h>
 #include <QObject>
 #include <QQmlEngine>
 
-class WindowLoggerModel : public QObject
+class WindowLoggerModel : public QObject,
+                          public ILoggerModel
+
 {
     Q_OBJECT
     QML_ELEMENT
+    Q_PROPERTY(QString path READ path WRITE setPath NOTIFY pathChanged)
+    Q_PROPERTY(QQueue<QString> messages READ messages WRITE setMessages
+                   NOTIFY messagesChanged)
+
 public:
     explicit WindowLoggerModel(QObject *parent = nullptr);
+    WindowLoggerModel(const WindowLoggerModel &other);
+
+    // ILoggerModel interface {
+    void setPath(const QString &path) override;
+    QString path() const override;
+    void setMessages(const QQueue<QString> &messages) override;
+    QQueue<QString> messages() const override;
+    void log(QString) override;
+    // }
+
+private:
+    QString m_path;
+    QQueue<QString> m_messages;
 
 signals:
-
+    void pathChanged(QString);
+    void messagesChanged(QQueue<QString>);
 };
 
 #endif // WINDOWLOGGERMODEL_H
