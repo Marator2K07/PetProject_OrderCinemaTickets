e171b8a633a64ca36fc68032e7f17da1b4eb019a
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-01-09 18:43:06 +0300

    Исправление интерфейса ILoggerModel.
    Т.к. теперь есть основной класс для лога Logger в библиотеке logger, то можно убрать лишние обязанности с данного интерфейса и все его потомков и отредактировать оставшиеся.

--- Desktop/backend/loggermodel/ILoggerModel.h
+++ Desktop/backend/loggermodel/ILoggerModel.h
@@ -1,23 +1,21 @@
 #ifndef ILOGGERMODEL_H
 #define ILOGGERMODEL_H
 
-#include <QQueue>
+#include <QString>
 
 ///
 /// \brief The ILoggerModel interface
-/// для организации функциональности логгера приложения
+/// для организации функциональности
+/// модели логгера приложения
 class ILoggerModel
 {
 public:
-    // каждый логгер содержит путь для сохранения данных
-    virtual void setPath(const QString &path) = 0;
-    virtual QString path() const = 0;
-    // каждый логгер содержит в себе произошедшие события
-    virtual void setMessages(const QQueue<QString> &messages) = 0;
-    virtual QQueue<QString> messages() const = 0;
+    // каждая модель логгера содержит в себе произошедшие события
+    virtual void setMessages(const QString &messages) = 0;
+    virtual QString messages() const = 0;
 
     // будущий слот принятия информационных сообщений
-    virtual void log(QString) = 0;
+    virtual void acceptMessage(QString) = 0;
 
     virtual ~ILoggerModel() {}
 };
