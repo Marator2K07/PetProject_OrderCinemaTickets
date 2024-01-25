878c425c549abc9e6b2215dd0f86a2ae8fe4d8cb
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-01-25 18:04:56 +0300

    Доработка класса ReplyWithIdentifier.
    Добавлены два основных поля, а также геттеры, сеттеры для них. Конструктор сразу инициализирует все поля по умолчанию. Еще был добавлен поясняющий комментарий для класса.

--- Desktop/backend/webinteraction/replywithidentifier.cpp
+++ Desktop/backend/webinteraction/replywithidentifier.cpp
@@ -2,5 +2,27 @@
 
 ReplyWithIdentifier::ReplyWithIdentifier(QObject *parent)
     : QObject{parent}
+    , reply{nullptr}
+    , identifier{RequestEnums::IDENTIFIERNONE}
 {
+}
+
+QNetworkReply *ReplyWithIdentifier::getReply() const
+{
+    return reply;
+}
+
+void ReplyWithIdentifier::setReply(QNetworkReply *newReply)
+{
+    reply = newReply;
+}
+
+RequestEnums::Identifier ReplyWithIdentifier::getIdentifier() const
+{
+    return identifier;
+}
+
+void ReplyWithIdentifier::setIdentifier(const RequestEnums::Identifier &newIdentifier)
+{
+    identifier = newIdentifier;
 }
--- Desktop/backend/webinteraction/replywithidentifier.h
+++ Desktop/backend/webinteraction/replywithidentifier.h
@@ -6,14 +6,25 @@
 #include <QNetworkReply>
 #include <QObject>
 
+///
+/// \brief The ReplyWithIdentifier class
+/// для совместного хранения двух важных
+/// связанных в контексте задачи обьектов
+/// (QNetworkReply и RequestEnums::Identifier)
 class ReplyWithIdentifier : public QObject
 {
     Q_OBJECT
 public:
     explicit ReplyWithIdentifier(QObject *parent = nullptr);
 
-signals:
+    QNetworkReply *getReply() const;
+    void setReply(QNetworkReply *newReply);
+    RequestEnums::Identifier getIdentifier() const;
+    void setIdentifier(const RequestEnums::Identifier &newIdentifier);
 
+private:
+    QNetworkReply *reply;
+    RequestEnums::Identifier identifier;
 };
 
 #endif // REPLYWITHIDENTIFIER_H
