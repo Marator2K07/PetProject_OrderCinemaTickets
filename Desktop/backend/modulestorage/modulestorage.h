fe28cb462356fcb4cc6013c31612810d7f3a2f31
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-01-27 17:49:26 +0300

    Update modulestorage.h
    Теперь есть поясняющий комментарий для класса. Также добавлено первое и единственное поле для хранения названий модулей.

--- Desktop/backend/modulestorage/modulestorage.h
+++ Desktop/backend/modulestorage/modulestorage.h
@@ -2,12 +2,19 @@
 #define MODULESTORAGE_H
 
 #include <QObject>
+#include <QHash>
 
+///
+/// \brief The ModuleStorage class
+/// класс-помощник для qml элемента Loader
 class ModuleStorage : public QObject
 {
     Q_OBJECT
 public:
     explicit ModuleStorage(QObject *parent = nullptr);
+
+private:
+    QHash<QString, QString> modules;
 };
 
 #endif // MODULESTORAGE_H
