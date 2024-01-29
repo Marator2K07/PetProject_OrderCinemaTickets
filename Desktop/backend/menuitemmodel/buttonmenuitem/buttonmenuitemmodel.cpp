6830fc0be167a3416611cfd1c63fcb3053b5058f
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-01-29 11:15:28 +0300

    Обновление класса buttonmenuitemmodel.
    Теперь данный класс является официальным наследником IMenuItemModel со всей соответствующей реализацией.

--- Desktop/backend/menuitemmodel/buttonmenuitem/buttonmenuitemmodel.cpp
+++ Desktop/backend/menuitemmodel/buttonmenuitem/buttonmenuitemmodel.cpp
@@ -4,3 +4,53 @@ ButtonMenuItemModel::ButtonMenuItemModel(QObject *parent)
     : QObject{parent}
 {
 }
+
+ButtonMenuItemModel::ButtonMenuItemModel(const ButtonMenuItemModel &other)
+    : m_text{other.text()}
+    , m_image{other.image()}
+    , m_simplifier{other.simplified()}
+{
+}
+
+void ButtonMenuItemModel::setText(const QString &text)
+{
+    if (text != m_text) {
+        m_text = text;
+        emit textChanged(text);
+    }
+}
+
+QString ButtonMenuItemModel::text() const
+{
+    return m_text;
+}
+
+void ButtonMenuItemModel::setImage(const QString &image)
+{
+    if (image != m_image) {
+        m_image = image;
+        emit imageChanged(image);
+    }
+}
+
+QString ButtonMenuItemModel::image() const
+{
+    return m_image;
+}
+
+void ButtonMenuItemModel::setSimplified(const bool &simplified)
+{
+    if (simplified != m_simplifier) {
+        m_simplifier = simplified;
+        emit simplifiedChanged(simplified);
+    }
+}
+
+bool ButtonMenuItemModel::simplified() const
+{
+    return m_simplifier;
+}
+
+ButtonMenuItemModel::~ButtonMenuItemModel()
+{
+}
--- Desktop/backend/menuitemmodel/buttonmenuitem/buttonmenuitemmodel.h
+++ Desktop/backend/menuitemmodel/buttonmenuitem/buttonmenuitemmodel.h
@@ -1,6 +1,7 @@
 #ifndef BUTTONMENUITEMMODEL_H
 #define BUTTONMENUITEMMODEL_H
 
+#include <IMenuItemModel.h>
 #include <QQmlEngine>
 #include <QObject>
 
@@ -8,12 +9,38 @@
 /// \brief The ButtonMenuItemModel class
 /// модель данных для работы с пунктом меню
 /// ведущим себя как обычная кнопка
-class ButtonMenuItemModel : public QObject
+class ButtonMenuItemModel : public QObject,
+                            public IMenuItemModel
 {
     Q_OBJECT
     QML_ELEMENT
+    Q_PROPERTY(QString text READ text WRITE setText NOTIFY textChanged)
+    Q_PROPERTY(QString image READ image WRITE setImage NOTIFY imageChanged)
+    Q_PROPERTY(bool simplified READ simplified WRITE setSimplified NOTIFY simplifiedChanged)
+
 public:
     explicit ButtonMenuItemModel(QObject *parent = nullptr);
+    ButtonMenuItemModel(const ButtonMenuItemModel &other);
+
+    // IMenuItemModel interface {
+    void setText(const QString &text) override;
+    QString text() const override;
+    void setImage(const QString &image) override;
+    QString image() const override;
+    void setSimplified(const bool &simplified) override;
+    bool simplified() const override;
+    ~ButtonMenuItemModel() override;
+    // }
+
+private:
+    QString m_text;
+    QString m_image;
+    bool m_simplifier;
+
+signals:
+    void textChanged(QString);
+    void imageChanged(QString);
+    void simplifiedChanged(bool);
 };
 
 #endif // BUTTONMENUITEMMODEL_H
