c4145f812c024e0309e011e8f90bc0864817ceb4
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-02-05 13:11:12 +0300

    Обновления в библиотеке listmodel.
    Линкуем данную библиотеку с menuitemmodel для работы с ней в классе menulistmodel.

--- Desktop/backend/listmodel/menulistmodel/menulistmodel.h
+++ Desktop/backend/listmodel/menulistmodel/menulistmodel.h
@@ -2,11 +2,15 @@
 #define MENULISTMODEL_H
 
 #include <QAbstractListModel>
+#include "IMenuItemModel.h"
 
 class MenuListModel : public QAbstractListModel
 {
 public:
     explicit MenuListModel(QObject *parent = nullptr);
+
+private:
+    QList<IMenuItemModel *> menuItems;
 };
 
 #endif // MENULISTMODEL_H
