8b898f0a3df1fc42cb415ddb34a017c6d48db971
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-02-05 13:47:17 +0300

    Обновления в классе menulistmodel.
    Как наследник QAbstractListModel - данный класс должен имплементировать реализацию данных методов rowCount, data, setData, flags.
    Пока пустая реализация.

--- Desktop/backend/listmodel/menulistmodel/menulistmodel.cpp
+++ Desktop/backend/listmodel/menulistmodel/menulistmodel.cpp
@@ -4,3 +4,23 @@ MenuListModel::MenuListModel(QObject *parent)
     : QAbstractListModel{parent}
 {
 }
+
+int MenuListModel::rowCount(const QModelIndex &parent) const
+{
+
+}
+
+QVariant MenuListModel::data(const QModelIndex &index, int role) const
+{
+
+}
+
+bool MenuListModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+
+}
+
+Qt::ItemFlags MenuListModel::flags(const QModelIndex &index) const
+{
+
+}
--- Desktop/backend/listmodel/menulistmodel/menulistmodel.h
+++ Desktop/backend/listmodel/menulistmodel/menulistmodel.h
@@ -9,6 +9,13 @@ class MenuListModel : public QAbstractListModel
 public:
     explicit MenuListModel(QObject *parent = nullptr);
 
+    // QAbstractItemModel interface
+    int rowCount(const QModelIndex &parent) const override;
+    QVariant data(const QModelIndex &index, int role) const override;
+    bool setData(const QModelIndex &index, const QVariant &value, int role) override;
+    Qt::ItemFlags flags(const QModelIndex &index) const override;
+    // }
+
 private:
     QList<IMenuItemModel *> menuItems;
 };
