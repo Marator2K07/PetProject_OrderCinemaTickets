e0f65ea2153a45b19313c3b77f352e0605554ff9
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-02-08 09:55:47 +0300

    Обновления в классе menulistmodel
    Все-таки метод flags пока не нужен. Так-же написаны стартовые реализации для всех оставшихся переопределенных методов.

--- Desktop/backend/listmodel/menulistmodel/menulistmodel.cpp
+++ Desktop/backend/listmodel/menulistmodel/menulistmodel.cpp
@@ -7,20 +7,37 @@ MenuListModel::MenuListModel(QObject *parent)
 
 int MenuListModel::rowCount(const QModelIndex &parent) const
 {
-
+    if (parent.internalPointer() == nullptr) {
+        return menuItems.count();
+    } else {
+        return 0;
+    }
 }
 
 QVariant MenuListModel::data(const QModelIndex &index, int role) const
 {
-
+    switch (role) {
+    case Qt::DisplayRole:
+        return menuItems.value(index.row())->text();
+    // another case:
+    }
+    return "";
 }
 
-bool MenuListModel::setData(const QModelIndex &index, const QVariant &value, int role)
+bool MenuListModel::setData(const QModelIndex &index,
+                            const QVariant &value,
+                            int role)
 {
-
-}
-
-Qt::ItemFlags MenuListModel::flags(const QModelIndex &index) const
-{
-
+    switch (role) {
+    case Qt::EditRole:
+        IMenuItemModel *menuItem = qvariant_cast<IMenuItemModel *>(value);
+        if (index.row() >= 0 && index.row() < menuItems.size()) {
+            menuItems.insert(index.row(), menuItem);
+        } else {
+            menuItems.append(menuItem);
+        }
+        return true;
+    // another case:
+    }
+    return false;
 }
--- Desktop/backend/listmodel/menulistmodel/menulistmodel.h
+++ Desktop/backend/listmodel/menulistmodel/menulistmodel.h
@@ -12,8 +12,9 @@ public:
     // QAbstractItemModel interface
     int rowCount(const QModelIndex &parent) const override;
     QVariant data(const QModelIndex &index, int role) const override;
-    bool setData(const QModelIndex &index, const QVariant &value, int role) override;
-    Qt::ItemFlags flags(const QModelIndex &index) const override;
+    bool setData(const QModelIndex &index,
+                 const QVariant &value,
+                 int role) override;
     // }
 
 private:
